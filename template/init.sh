930e8c5b4339fcf349153e289a1f6e98ea9de65c
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2024-06-11 14:00:08 +0200

    Set all environment variables used for parallelization to 1.

--- template/init.sh
+++ template/init.sh
@@ -8,3 +8,10 @@ mkdir -p "$TMPDIR"
 
 # Ensure that the latest documentation is built.
 pixi run build_docs
+
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+export BLIS_NUM_THREADS=1
+export VECLIB_MAXIMUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
